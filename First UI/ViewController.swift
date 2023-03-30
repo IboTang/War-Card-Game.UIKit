86216c33dee39e75ca454647411db26adb15c67f
Ibragim Tanshanlo <tang.ibo@hotmail.com>
2023-03-30 17:51:00 +0600

    Initial Commit

---
+++ First UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First UI
+//
+//  Created by Ibo Tang on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
